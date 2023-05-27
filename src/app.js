753e39836c5e8df5f11d47ac6ea58066659e2b47
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-05-27 03:38:32 +0000

    Nuevo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let icons = [
+  { key: "♦", color: "red" },
+  { key: "♥", color: "red" },
+  { key: "♠", color: "black" },
+  { key: "♣", color: "black" }
+];
+
+let simbolTop = document.querySelector(".simbol");
+let simbolBot = document.querySelector(".simbol2");
+let cardNumber = document.querySelector(".number");
+let button = document.querySelector(".btn");
+
+button.addEventListener("click", generateCard);
+
+function generateCard() {
+  let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+  simbolTop.innerHTML = randomIcon.key;
+  simbolBot.innerHTML = randomIcon.key;
+  simbolTop.style.color = randomIcon.color;
+  simbolBot.style.color = randomIcon.color;
+  cardNumber.innerHTML = randomNum;
+}
